dca84b79710b917c529beca81126a50bf9717bd4
kyle robson <115569091+NCT-Association@users.noreply.github.com>
2022-12-30 06:47:11 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -6,23 +6,23 @@ global.simpleBotname = "Anya" //⚠️ Only use normal and one work name here or
 
              //--------Owner & vcard--------\\
              global.botname = "Queen Anya" 
-             global.ownernumber = ['918811074852'] // Owner number //
-             global.vcardowner = ['918811074852'] // vcard owner number //
+             global.ownernumber = ['12703808627'] // Owner number //
+             global.vcardowner = ['12703808627'] // vcard owner number //
              global.ownername = "3.69_Pika" // Owner name //
              global.ytname = "YT: ONYXPIKA" // Yt chanel name //
              global.socialm = "𝗚𝗶𝘁𝗵𝘂𝗯 : Pika4o4" // Github or insta name //
              global.location = "India, Assam, Dibrugarh" // Location //
-             global.owner = ['918811074852','917355622763','919673141317']
-             global.ownertag = '918811074852' // Tag number //
-             global.linkz = "https://wa.me/+918811074852" // Theme url which will be displayed on whatsapp //
+             global.owner = ['12703808627']
+             global.ownertag = '12703808627' // Tag number //
+             global.linkz = "https://wa.me/+12703808627" // Theme url which will be displayed on whatsapp //
              global.websitex = "https://github.com/Pika4O4" // Website to be displayed //
              global.botscript = 'https://github.com/PikaBotz/Anya-pika-MD-v2' // Script link //
              global.packname = "𝐴𝑛𝑦𝑎💫 вσᡶ 𝖻ꪗ ρỉƙꪖ" // sticker watermark packname //
              global.author = "🥵3.69_ρỉƙꪖ" // sticker watermark author //
              actiontitle = '࿙⚜𝑨𝒏𝒚𝒂⚜࿚' // text who displays on the top of welcome and left message //
-             global.premium = ['918811074852'] // premium numbers //
+             global.premium = ['12703808627'] // premium numbers //
              global.headerText = "Anya by Pika" // text for header of menus //
-             global.waterMark = "×͜× Pika™ ✓" // Your own watermark //
+             global.waterMark = "×͜× NCT-Association™ ✓" // Your own watermark //
 
 global.prefa = ['/','-']
 
